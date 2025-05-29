dc0fc56cf367feb0463e3cfeaedb29d5c5e60c75
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2025-05-29 22:42:13 +0200

    reprise du backend

--- backend/studialink/core/permissions.py
+++
@@ -1,6 +0,0 @@
-# core/permissions.py
-from rest_framework import permissions
-
-class IsOwnerOrAdmin(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj == request.user or request.user.administrateur
